dc5be9b46313852a3709019a489e2fdd47e12abd
Sejoon <sjchang21@student.kis.or.kr>
2021-06-26 14:10:29 +0900

    comment feature

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from django.urls import path
 from . import views
 urlpatterns = [
@@ -5,4 +6,9 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name="post_draft_list"),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name="comment_remove"),
+    path('comment/<int:pk>/approve/', views.comment_approve, name="comment_approve"),
 ]
